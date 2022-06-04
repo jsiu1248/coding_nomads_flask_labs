ed6f87cfda2e57641f58358ad5747f0f18fba531
Jonathan Siu <jsiu1248@gmail.com>
2022-06-04 12:12:50 -0700

    need to understand addresses more

--- 05_first_flask_app/01_fix_the_routes/app.py
+++ 05_first_flask_app/01_fix_the_routes/app.py
@@ -6,14 +6,19 @@ app = Flask(__name__)
 @app.route('/')
 def welcome():
     page = "Welcome to Emily's Dog Costumes!"
+    return page
 
 
-@app.route('services')
+@app.route('/services')
 def services():
     page = "I offer custom made costumes for your precious canine companion, "\
         "and a free in-home consultation, to get the measurements."
+    return page
 
-
-@app.route('/costumes/costume')
+@app.route('/costumes/<costume>')
 def costumes(costume):
     page = f"Check out this {costume} costume!"
+    return page
+
+if __name__ =='main':
+    app.run(host="localhost", port=8000, debug=True)
