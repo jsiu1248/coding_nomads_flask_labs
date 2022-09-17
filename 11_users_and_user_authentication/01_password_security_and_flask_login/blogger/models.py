8c4cc748e7e83cd865ac30590612b5a8c75a8d1d
Jonathan Siu <jsiu1248@gmail.com>
2022-09-17 11:21:57 -0700

    added password, password setter. and password verify

--- 11_users_and_user_authentication/01_password_security_and_flask_login/blogger/models.py
+++ 11_users_and_user_authentication/01_password_security_and_flask_login/blogger/models.py
@@ -12,16 +12,32 @@ class User(db.Model):
     firstname = db.Column(db.String(50))
     lastname = db.Column(db.String(50))
     username = db.Column(db.String(50), unique=True, index=True)
-    password = db.Column(db.String(50))
+    password_hash = db.Column(db.String(50))
     email = db.Column(db.String(50), unique=True, index=True)
     dateofreg = db.Column(db.DateTime, default=datetime.datetime.now)
 
+    # should password be take out of this? Can it be queried?
     def __init__(self, firstname, lastname, username, password, email):
         self.firstname = firstname
         self.lastname = lastname
         self.username = username
         self.password = password
         self.email = email
+    
+    # errors out when someone tries to read it
+    @property
+    def password(self):
+        raise AttributeError('password is not readable')
+
+    # allow user to write password
+    @password.setter
+    def password(self, password):
+       # flask already has a function that helps with hashing and adding salt
+        self.password_hash = generate_password_hash(password)
+
+    # it takes the password and hash together and returns true if correct
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Post(db.Model):
