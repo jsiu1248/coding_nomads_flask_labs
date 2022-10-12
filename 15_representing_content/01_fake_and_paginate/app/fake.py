b841c22a9cc67a0dc30ce0d494e59223dab5a149
Jonathan Siu <jsiu1248@gmail.com>
2022-10-11 22:38:55 -0700

    added todos

--- 15_representing_content/01_fake_and_paginate/app/fake.py
+++ 15_representing_content/01_fake_and_paginate/app/fake.py
@@ -1,8 +1,8 @@
 from sqlalchemy.exc import IntegrityError
 from faker import Faker
 from . import db
-from .models import User, TodoList
-from random import randint
+from .models import User, TodoList, Todo
+from random import randint, choices
 import string
 
 def users(count=10):
@@ -32,19 +32,21 @@ def todolists(count = 75):
         # offset dicards a certain number of results, n is a random number between 0 and then user_count -1
         # so it is picking a random user and don't care about duplicated users because users can have multiple compositions
         u = User.query.offset(randint(0, user_count - 1)).first()
-        c = TodoList(title = string.capwords(fake.bs()),
+        t = TodoList(title = string.capwords(fake.bs()),
                     create_at = fake.past_date(), 
                     creator = u)
-        db.session.add(c)
+        db.session.add(t)
     db.session.commit()
 
 def todos(count = randint(5,75)):
     fake = Faker()
-    Todo(description = , 
-        created_at = fake.past_data(),
-        finished_at= fake.past_data(), 
-        is_finished= , 
-        creator= u, 
-        todolist_id=)
-
-        # how to get 5 to 75 tasks per list?
+    user_count = User.query.count()
+    for i in range(count):
+        u = User.query.offset(randint(0, user_count - 1)).first()
+        task = Todo(description =fake.text(), 
+            created_at = fake.past_data(),
+            finished_at= fake.past_data(), 
+            is_finished= choices([True, False], [0.5, 0.5]), 
+            creator= u)
+        db.session.add(task)
+    db.session.commit()
